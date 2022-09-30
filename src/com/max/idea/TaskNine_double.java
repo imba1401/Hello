3fa1b783ace0da22507f9baf8b4f5ad29edcf273
imba1401 <imba_olga@mail.ru>
2022-09-30 16:32:20 +0300

    Задание №9_Циклы

---
+++ src/com/max/idea/TaskNine_double.java
@@ -0,0 +1,57 @@
+package com.max.idea;
+import java.util.Scanner;
+
+public class TaskNine_double {
+
+    public static void main(String[] args) {
+
+        //1. Пользователь вводит размер массива и данные с клавиатуры в массив типа double.
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int operand1 = in.nextInt();
+
+
+        double [] arr = new double [operand1];
+
+        for (int i = 0; i <= (operand1 - 1); i++) {
+            System.out.println("Введите число (одно):");
+            double value = in.nextDouble();
+            arr[i] = value;
+        }
+
+        double total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        System.out.println("Сумма элементов массива: " + total);
+
+      //2. Посчитайте среднее арифметическое элементов массива.
+        double average;
+        average = total/arr.length;
+        System.out.println("Среднее арифметическое: " + average);
+
+     //3. После этого произведите вывод массива на экран
+        // где каждый элемент массива умножается на среднее арифметическое
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println( arr[i] + "*" + average);
+        }
+
+
+        // String str = Arrays.deepToString(array23);
+        //  System.out.println(str);
+
+
+        //    int total = 0;
+        // Прибавляем к переменной total каждый элемент массива.
+        //    for (int i = 0; i < arr.length; i++) {
+
+        //   total += arr[i];
+
+
+
+    }
+}
+
+
