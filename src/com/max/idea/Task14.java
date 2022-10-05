f12e2ebb69803978898699b2c626a3dec052cf65
imba1401 <imba_olga@mail.ru>
2022-10-05 19:55:14 +0300

    Задание №14_Работа с данными

--- src/com/max/idea/Task14.java
+++ src/com/max/idea/Task14.java
@@ -11,7 +11,6 @@ public class Task14 {
 
         //2. Из максимального и минимального значения выведите наибольшее по модулю.
 
-
         Random rand = new Random();
 
         int[] array = new int[15];
